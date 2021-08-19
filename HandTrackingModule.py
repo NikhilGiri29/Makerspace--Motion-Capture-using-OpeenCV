408c702f845d980117f19b26f694d9562a23f979
Nikhil Giri <nikhu.irig29@gmail.com>
2021-08-19 23:36:43 +0530

    removed the test program from HandTrackingModule.py

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -118,33 +118,10 @@ class handsDetector():
 
 
 def main():
-    #### A test program can be ran in another py file with suitable imports ####
-    '''
-    pTime = 0
-    cTime = 0
-
-    cap = cv2.VideoCapture(0)
-    detector = handsDetector()
-    while True:
-        success, img = cap.read()
-        img = detector.findHands(img)
-        lmList = detector.findPosition(img)
-        if len(lmList) != 0:
-            print(lmList[4])
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
-
-        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-
-        cv2.imshow("Image", img)
-        k = cv2.waitKey(1)
-        if k == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
-    '''
+    print("Please dont run this file!!!!")
+    print("This is a package, it contains functions for your projects")
+    print("Download it and import the handsDetector() class in another python project")
+    print("Enjoy!!!")
 
 
 if __name__ == "__main__":
